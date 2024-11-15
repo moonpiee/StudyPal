d19b8ac87bf7b20f2abec9939fc7956c254ccd0a
ChandPie_thecod(h)er <96528075+moonpiee@users.noreply.github.com>
2024-11-15 14:50:50 +0530

    Add files via upload

---
+++ Studypal.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+session_id_runn="runn_chat_history"
+session_id_wo_runn="wo_runn_chat_history"
+st.title("Study Pal🌱📚💪")
+st.subheader("Learning is Fun!")
+languages=["English","Telugu","Hindi","Malayalam","Marathi","Bengali","Kannada"]
+instructions_lis=["Tutor Me","Teacher Pal","Translate","Learn Language","Communicate","Surprise Me","Tell Me Why","Tongue Twister","Humor Me","Story Time", "More Fun Games","Inspire Me"]
+instruction = st.selectbox("Pages",instructions_lis)
